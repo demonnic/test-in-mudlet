6248474074f017aee4631b520d1c28971363aa93
Damian <demonnic@gmail.com>
2022-05-20 14:22:23 -0400

    Try caching the mudlet executable

--- download-mudlet.sh
+++ download-mudlet.sh
@@ -1,5 +1,5 @@
 echo "Requested download of Mudlet version: $1"
-cd $GITHUB_WORKSPACE
+cd /tmp
 if [[ "$1" == "LATEST" ]]; then
   VERSION=`curl --silent "https://api.github.com/repos/mudlet/mudlet/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'`
 else
