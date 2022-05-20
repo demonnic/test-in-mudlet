2aa3f51ac3dc4c9b0a9ace79538688283441cb16
Damian <demonnic@gmail.com>
2022-05-20 14:42:40 -0400

    Clean up output

--- download-mudlet.sh
+++ download-mudlet.sh
@@ -2,6 +2,5 @@ echo "Requested download of Mudlet version: $1"
 cd /tmp
 VERSION=$1
 
-echo "Downloading Mudlet version: $VERSION"
 curl -L "https://www.mudlet.org/wp-content/files/$VERSION-linux-x64.AppImage.tar" --output Mudlet.tar &&\
 tar xf Mudlet.tar
